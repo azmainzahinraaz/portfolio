794c0c90c383668b0a2696570fa0d790f5fae2e5
Rakesh Karmaker <rakeshkarmaker0175@gmail.com>
2025-12-21 16:47:14 +0600

    feat: update index.html title and structure, remove unused App component, and add Netlify configuration files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router/router.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+
+import "./index.css";
 
 const queryClient = new QueryClient();
 
