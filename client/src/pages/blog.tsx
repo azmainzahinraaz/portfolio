cf998f8df021a4df1ab3709d18f7bb30da526d5c
Rakesh Karmaker <rakeshkarmaker0175@gmail.com>
2026-01-21 19:35:53 +0600

    feat: add blog section with BlogCard component and sample blog data for enhanced content display

--- client/src/pages/blog.tsx
+++ client/src/pages/blog.tsx
@@ -1,3 +1,6 @@
+import BlogCard from "@/components/blogs-components/blog-card";
+import BlockText from "@/components/ui/block-text";
+import { blogs } from "@/services/data/blog-data";
 import { useEffect, type ReactNode } from "react";
 import { Helmet } from "react-helmet-async";
 
@@ -12,10 +15,31 @@ export default function Blog(): ReactNode {
         <title>Azmain Zahin Raaz - Blog</title>
       </Helmet>
 
-      <iframe
+      {/* <iframe
         src="https://boishakhionline.com/education/156584"
         className="block w-full h-[calc(100vh-4.75rem)]"
-      ></iframe>
+      ></iframe> */}
+
+      <section className="w-full h-full flex justify-center pb-17">
+        <div className="w-full h-full max-w-small-max-width flex flex-col gap-10">
+          <div
+            className="w-full h-71.5 relative after:absolute after:w-full after:h-[80%] after:bottom-0 after:bg-linear-to-t after:from-primary-bg-color after:from-10% after:to-transparent"
+            style={{
+              backgroundImage: "url('coverphoto-work-1.jpg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+          <div className="w-full h-full flex flex-col gap-10">
+            <BlockText className="ml-5">My Blogs</BlockText>
+            <div className="w-full h-full grid grid-cols-3 max-xl:grid-cols-2 max-sm:grid-cols-1 gap-7">
+              {blogs.map((blog) => (
+                <BlogCard key={blog._id} blog={blog} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
---
+++ client/src/services/data/blog-data.ts
@@ -0,0 +1,38 @@
+export type BlogType = {
+  _id: string;
+  imageUrl: string;
+  date: string;
+  publisher: string;
+  title: string;
+  link: string;
+};
+
+export const blogs: BlogType[] = [
+  {
+    _id: "1",
+    imageUrl:
+      "https://boishakhionline.com/assets/news_photos/2026/01/14/image-156584-1768402039.jpg",
+    date: "January 14, 2026",
+    publisher: "Boishakhi Online",
+    title: "Azmain Zahin Raaz Wins Silver in Olympiad",
+    link: "https://boishakhionline.com/education/156584",
+  },
+  {
+    _id: "2",
+    imageUrl:
+      "https://boishakhionline.com/assets/news_photos/2026/01/14/image-156584-1768402039.jpg",
+    date: "January 14, 2026",
+    publisher: "Boishakhi Online",
+    title: "Azmain Zahin Raaz Wins Silver in Olympiad",
+    link: "https://boishakhionline.com/education/156584",
+  },
+  {
+    _id: "3",
+    imageUrl:
+      "https://boishakhionline.com/assets/news_photos/2026/01/14/image-156584-1768402039.jpg",
+    date: "January 14, 2026",
+    publisher: "Boishakhi Online",
+    title: "Azmain Zahin Raaz Wins Silver in Olympiad",
+    link: "https://boishakhionline.com/education/156584",
+  },
+];
