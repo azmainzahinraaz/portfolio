ee49fab428944f40805577405046e286538f9be8
Rakesh Karmaker <rakeshkarmaker0175@gmail.com>
2026-02-21 03:47:19 +0600

    refactor: remove Facebook data fetching and related components

--- client/src/lib/api/facebook-data.ts
+++
@@ -1,7 +0,0 @@
-import { FACEBOOK_API_URL } from "@/config/constants";
-import axios from "axios";
-
-export async function fetchFacebookVideos() {
-  const data = await axios.get(FACEBOOK_API_URL);
-  return data.data;
-}
